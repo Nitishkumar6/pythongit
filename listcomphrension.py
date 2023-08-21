f229712e55ade01135030915f7856317634db36e
Nitishkumar6 <np.nitishkumar@gmail.com>
2023-08-21 12:24:37 +0530

    python

---
+++ listcomphrension.py
@@ -0,0 +1,7 @@
+#syntax= i for i in range(100)
+
+res=["even" if i%2==0 else "odd" for i in range(10)]
+print(res)
+
+result=[i for i in range(10)]
+print(result)
---
+++ set.py
@@ -0,0 +1,8 @@
+set={1,2,3,4,}
+data=int(input("enter the data"))
+set.add(data)
+print(set)
+
+
+#set does not allow duplicate values
+#it is unordered collection data
